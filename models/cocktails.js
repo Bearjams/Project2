3af7350becde78e170de1d99de46b6692e206e42
Nick <nicholasroman86@gmail.com>
2020-07-29 19:52:27 -0400

    Removed erroneous code and corrected grammatical errors

--- models/cocktails.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var searchHistory = sequelize.define("searchHistory", {
-      Search: DataTypes.STRING,
-      Name: DataTypes.STRING,
-      instructions: DataTypes.TEXT,
-      ingredients: DataTypes.STRING
-    });
-    return searchHistory;
-  };
